f6cfd771d020057e0985e89f0d7f00dafc8fbb6b
Svetlana Seliverstova <zhuldybina1594@yandex.ru>
2023-07-27 02:22:12 +0300

    Вторая итерация

---
+++ pages/index.js
@@ -0,0 +1,30 @@
+const popupElement = document.querySelector('.popup');
+const formElement = popupElement.querySelector('.popup__container');
+let closeIcon = formElement.querySelector('.popup__close-icon');
+let infoIcon = document.querySelector('.info__edit-button');
+let infoName = document.querySelector('.info__name');
+let infoAbout = document.querySelector('.info__about');
+let nameInput = formElement.querySelector('.popup__field_type_name');
+let jobInput = formElement.querySelector('.popup__field_type_description');
+
+function openPopup() {
+    popupElement.classList.add('popup_opened');
+    nameInput.value = infoName.textContent;
+    jobInput.value = infoAbout.textContent;
+  }
+
+function closePopup() {
+    popupElement.classList.remove('popup_opened');
+  }
+  
+infoIcon.addEventListener ('click', openPopup);
+closeIcon.addEventListener ('click', closePopup);
+
+function handleFormSubmit (evt) {
+  evt.preventDefault();
+  infoName.textContent = nameInput.value;
+  infoAbout.textContent = jobInput.value;
+  closePopup();
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
