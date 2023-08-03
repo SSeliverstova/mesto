563e2a2681dfdcd416c647cedd34f666e46a144a
Svetlana Seliverstova <zhuldybina1594@yandex.ru>
2023-08-03 23:10:14 +0300

    Третья итерация

--- components/index.js
+++ components/index.js
@@ -4,24 +4,24 @@ const popupContainerEditProfile = popupElementEditProfile.querySelector('.popup_
 const formElementEditProfile = popupElementEditProfile.querySelector('.popup__form');
 const closeIconEditProfile = popupContainerEditProfile.querySelector('.popup__close-icon');
 const infoIcon = document.querySelector('.info__edit-button');
-let infoName = document.querySelector('.info__name');
-let infoAbout = document.querySelector('.info__about');
-let nameInput = formElementEditProfile.querySelector('.popup__field_type_name');
-let jobInput = formElementEditProfile.querySelector('.popup__field_type_description');
+const infoName = document.querySelector('.info__name');
+const infoAbout = document.querySelector('.info__about');
+const nameInput = formElementEditProfile.querySelector('.popup__field_type_name');
+const jobInput = formElementEditProfile.querySelector('.popup__field_type_description');
 
 const popupElementAddCards = document.querySelector('.popup_type_add-cards');
 const popupContainerAddCards = popupElementAddCards.querySelector('.popup__container');
 const formElementAddCards = popupElementAddCards.querySelector('.popup__form');
 const closeIconAddCards = popupContainerAddCards.querySelector('.popup__close-icon');
 const addButton = document.querySelector('.profile__add-button');
-let nameCard = formElementAddCards.querySelector('.popup__field_type_card-name');
-let linkCard = formElementAddCards.querySelector('.popup__field_type_card-link');
+const nameCard = formElementAddCards.querySelector('.popup__field_type_card-name');
+const linkCard = formElementAddCards.querySelector('.popup__field_type_card-link');
 
 const popupElementPhoto = document.querySelector('.popup_type_photo');
 const popupContainerPhoto = popupElementPhoto.querySelector('.popup__container');
 const closeIconPhoto = popupContainerPhoto.querySelector('.popup__close-icon');
-let photoName = popupContainerPhoto.querySelector('.popup__photo-name');
-let photoImage = popupContainerPhoto.querySelector('.popup__image');
+const photoName = popupContainerPhoto.querySelector('.popup__photo-name');
+const photoImage = popupContainerPhoto.querySelector('.popup__image');
 
 
 const cards = document.querySelector('.elements');
@@ -30,8 +30,8 @@ function openPopup(popup) {
   popup.classList.add('popup_opened');
 }
 
-function closePopup() {
-  document.querySelector('.popup_opened').classList.remove('popup_opened');
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
 }
 
 function editProfile() {
@@ -47,15 +47,15 @@ function addCard() {
 
 addButton.addEventListener ('click', addCard);
 infoIcon.addEventListener ('click', editProfile);
-closeIconEditProfile.addEventListener ('click', closePopup);
-closeIconAddCards.addEventListener ('click', closePopup);
-closeIconPhoto.addEventListener ('click', closePopup);
+closeIconEditProfile.addEventListener ('click', () => { closePopup(popupElementEditProfile) });
+closeIconAddCards.addEventListener ('click', () => { closePopup(popupElementAddCards) });
+closeIconPhoto.addEventListener ('click', () => { closePopup(popupElementPhoto) });
 
 function renameProfile (evt) {
   evt.preventDefault();
   infoName.textContent = nameInput.value;
   infoAbout.textContent = jobInput.value;
-  closePopup();
+  closePopup(popupElementEditProfile);
 }
 
 formElementEditProfile.addEventListener('submit', renameProfile);
@@ -89,7 +89,7 @@ const createCard = (link, name) => {
 formElementAddCards.addEventListener('submit', function(evt) {
   evt.preventDefault();
   cards.prepend(createCard(linkCard.value, nameCard.value));
-  closePopup();
+  closePopup(popupElementAddCards);
 
 });
 
